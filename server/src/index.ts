48c0212f92f4e28dae651a32700183ea51a0dee5
s0lix-x <dgarou2@gmail.com>
2026-09-12 00:19:44 +0700

    feat: add mobile login

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,7 +2,8 @@ import express from "express";
 import authRouter from "./routes/auth/auth.route";
 import postsRouter from "./routes/posts/posts.route";
 import usersRouter from "./routes/users/users.route";
-import categoriesRouter from "./routes/categories/categories.route"
+import categoriesRouter from "./routes/categories/categories.route";
+import cors from "cors";
 
 const app = express();
 const PORT = 5000;
@@ -15,9 +16,10 @@ app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
 
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 
-app.use('/api/v1/auth', authRouter);
+app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/posts", postsRouter);
 app.use("/api/v1/users", usersRouter);
 app.use("/api/v1/categories", categoriesRouter);
