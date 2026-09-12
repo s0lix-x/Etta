e534c22f6b04bdbfb13e5e96fc7fb3a8e0914fdb
s0lix-x <dgarou2@gmail.com>
2026-09-12 22:38:34 +0700

    feat: add feed page

--- blogapp/lib/widgets/bottombarwidget.dart
+++ blogapp/lib/widgets/bottombarwidget.dart
@@ -1,4 +1,5 @@
 
+import 'package:blogapp/pages/feedpage.dart';
 import 'package:flutter/material.dart';
 import 'package:persistent_bottom_nav_bar_v2/persistent_bottom_nav_bar_v2.dart';
 
@@ -9,66 +10,10 @@ class BottomBarWidget extends StatelessWidget {
   List<PersistentTabConfig> _tabs() {
     return [
       PersistentTabConfig(
-        screen: const HomePage(),
+        screen: const FeedPage(),
         item: ItemConfig(
           icon: const Icon(Icons.menu_book_outlined, size: 24),
           title: "FEED",
-          activeForegroundColor: const Color(0xffb84a2a),
-          inactiveForegroundColor: const Color(0xff666666),
-          textStyle: const TextStyle(
-            fontSize: 11,
-            fontWeight: FontWeight.w500,
-            letterSpacing: 1,
-          ),
-        ),
-      ),
-      PersistentTabConfig(
-        screen: const MessagePage(),
-        item: ItemConfig(
-          icon: const Icon(Icons.explore_outlined, size: 24),
-          title: "DISCOVER",
-          activeForegroundColor: const Color(0xffb84a2a),
-          inactiveForegroundColor: const Color(0xff666666),
-          textStyle: const TextStyle(
-            fontSize: 11,
-            fontWeight: FontWeight.w500,
-            letterSpacing: 1,
-          ),
-        ),
-      ),
-      PersistentTabConfig(
-        screen: const SearchPage(),
-        item: ItemConfig(
-          icon: const Icon(Icons.edit_outlined, size: 24),
-          title: "WRITE",
-          activeForegroundColor: const Color(0xffb84a2a),
-          inactiveForegroundColor: const Color(0xff666666),
-          textStyle: const TextStyle(
-            fontSize: 11,
-            fontWeight: FontWeight.w500,
-            letterSpacing: 1,
-          ),
-        ),
-      ),
-      PersistentTabConfig(
-        screen: const NotificationPage(),
-        item: ItemConfig(
-          icon: const Icon(Icons.grid_view_outlined, size: 24),
-          title: "CATEGORIES",
-          activeForegroundColor: const Color(0xffb84a2a),
-          inactiveForegroundColor: const Color(0xff666666),
-          textStyle: const TextStyle(
-            fontSize: 11,
-            fontWeight: FontWeight.w500,
-            letterSpacing: 1,
-          ),
-        ),
-      ),
-      PersistentTabConfig(
-        screen: const ProfilePage(),
-        item: ItemConfig(
-          icon: const Icon(Icons.person_outline, size: 24),
-          title: "PROFILE",
           activeForegroundColor: const Color(0xffb84a2a),
           inactiveForegroundColor: const Color(0xff666666),
           textStyle: const TextStyle(
